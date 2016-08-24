8afa29dab44e1d7195c3a2fadc939c7da9f74b73
pspeiser <pat@patspeiser.com>
2016-08-23 20:18:10 -0400

    db data rendering to page

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var volleyball = require('volleyball');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var Model = require('./models');
+
+var Hotel = Model.hotel;
+var Restaurant = Model.restaurant;
+var Activity = Model.activity;
 
 swig.setDefaults({cache: false});
 app.set('views', __dirname + '/views');
@@ -11,15 +16,20 @@ app.engine('html', swig.renderFile);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(volleyball);
-
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(err, req, res, next) {
 	console.log("Oh noes!!!!!");
 	console.log(err, err.stack);
 });
+
+app.get('/', function(req, res){
+		Promise.all([Hotel.findAll({}), Restaurant.findAll({}), Activity.findAll({})])	
+		.then( function(allData){
+			res.render('index', { Hotels: allData[0], Restaurants: allData[1], Activities: allData[2]  });
+		})
+})
 
 app.listen(3000, function() {
 	console.log("Server is listening intently at port 3000...")
